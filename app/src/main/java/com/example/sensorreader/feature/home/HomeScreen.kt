7bb0915488cabc2616876ad839287071baa953d3
Pavel Antoshkin <34233903+Papashkin@users.noreply.github.com>
2024-12-14 22:24:13 +0100

    task: final clean-up (#9)
    Co-authored-by: hedfan <muha2410@mail.ru>

--- app/src/main/java/com/example/sensorreader/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/sensorreader/feature/home/HomeScreen.kt
@@ -1,25 +1,19 @@
 package com.example.sensorreader.feature.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
